fb3ea9ead3d02423b5041d63049e714af77ba435
RAGAVENDAR123 <raguragavendar@gmail.com>
2016-08-13 13:05:23 +0530

    Update TEST.C

--- TEST.C
+++ TEST.C
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main()
+{
+  int a;
+  sacnf("%d".&a);
+  if(a>0)
+  {
+    printf("Positive number");
+    else
+    printf("Negative Number");
+  }
+  if(a==0)
+  printf("BOundary condition")
+}
